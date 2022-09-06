ea4d536ed28da12e0eed5bf564e68d1862392b1a
Beta-Tek <connerbedard@gmail.com>
2022-09-06 19:19:12 -0400

    Removed lockon header.

--- Lockon S3/LockOn Data.asm
+++ Lockon S3/LockOn Data.asm
@@ -1,14 +1,3 @@
-LockOnROM_Start:
-		binclude "Lockon S3/Header.bin"
-		even
-
-SSMagic_TestLoc_200114		EQU LockOnROM_Start+$114
-SSMagic_TestLoc_20011A		EQU LockOnROM_Start+$11A
-LockonDate			EQU LockOnROM_Start+$11C
-SSMagic_TestLoc_200150		EQU LockOnROM_Start+$150
-LockonSerialNumber		EQU LockOnROM_Start+$180
-SSMagic_TestLoc_2001A4		EQU LockOnROM_Start+$1A4
-
 Pal_Competition1:
 		binclude "General/Competition Menu/Palettes/1.bin"
 		even
